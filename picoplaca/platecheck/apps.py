cbe4a9d8018d3327f73dd15947e13ba6ea6f0e27
palpico <32603826+palpico@users.noreply.github.com>
2021-04-05 10:44:43 -0500

    add model to admin add verbose name to app

--- picoplaca/platecheck/apps.py
+++ picoplaca/platecheck/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class PlatecheckConfig(AppConfig):
-    name = 'platecheck'
+    name = 'picoplaca.platecheck'
+    verbose_name = _("Plate Check")
