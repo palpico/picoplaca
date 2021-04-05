4130f940725784a500180618d63eb10419135780
palpico <32603826+palpico@users.noreply.github.com>
2021-04-05 10:58:50 -0500

    add CBV for platecheck add plate validation logic add html template, view and url

--- picoplaca/platecheck/views.py
+++ picoplaca/platecheck/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.utils.datetime_safe import datetime
+from picoplaca.platecheck.models import PlateCheck
+from django.views.generic import CreateView
+from django.shortcuts import redirect
 
-# Create your views here.
+
+class PlateCheckCV(CreateView):
+    model = PlateCheck
+    fields = "__all__"
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        self.request.session['g_result'] = picoplaca(obj.platecheck_plate, obj.platecheck_date, obj.platecheck_time)
+        return redirect('platecheck:result')
+
+
+def check_date(arg_plate, arg_date):
+    # dict for weekdays and last digit plate
+    day_plates = {"1": ["1", "2"], "2": ["3", "4"], "3": ["5", "6"], "4": ["7", "8"], "5": ["9", "0"]}
+    if arg_plate[-1] in day_plates[arg_date.strftime("%w")]:
+        return True
+    else:
+        return False
+
+
+def time_range(begin_time, end_time, check_time):
+    if datetime.strptime(begin_time, '%H:%M:%S').time() <= check_time <= datetime.strptime(end_time, '%H:%M:%S').time():
+        return True
+    else:
+        return False
+
+
+def picoplaca(arg_plate, arg_date, arg_time):
+    if time_range("7:00:00", "9:30:00", arg_time) is False and time_range("16:00:00", "19:30:00", arg_time) is False:
+        return True
+    elif check_date(arg_plate, arg_date):
+        return False
+    else:
+        return True
