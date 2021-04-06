ff35aa129b5fb63a4ec63b4e9e3261ed973e9496
palpico <32603826+palpico@users.noreply.github.com>
2021-04-06 02:50:31 -0500

    changed home url

--- picoplaca/platecheck/urls.py
+++ picoplaca/platecheck/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from picoplaca.platecheck.views import PlateCheckCV
 
 app_name = "platecheck"
 urlpatterns = [
-    path("", PlateCheckCV.as_view(), name="check"),
     path('result/', TemplateView.as_view(template_name='platecheck/results.html'), name='result'),
 ]
